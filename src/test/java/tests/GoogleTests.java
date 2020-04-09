61f9bca8b9c244061e64a5bb8e70e6a41d8909d4
dilixat928@gmail.com <dilixat928@gmail.com>
2020-04-08 18:35:50 -0700

    add paralle

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -2,6 +2,7 @@ package tests;
 
 public class GoogleTests {
   //this is a comment
+
   public static void main(String[] args) {
       System.out.println("dilara");
       System.out.println("dell");
